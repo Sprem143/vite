49d8059eb405a8745cb513144a92ab821c6e39a9
Prem Kumar <prem68265@gmail.com>
2023-10-31 18:48:14 +0530

    third changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React_+ Prem</h1>
+      <h2>THis is my first react app with vite</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
